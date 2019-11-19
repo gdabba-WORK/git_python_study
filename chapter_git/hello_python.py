e4cffc2497cae2b48673a98bd1219a302426a92c
인공지능 <kgdfather@gmail.com>
2019-11-19 10:34:55 +0900

    div(a, b)함수 추가, main print문 추가

--- chapter_git/hello_python.py
+++ chapter_git/hello_python.py
@@ -1,12 +1,15 @@
 def add(a, b):
-    return a+b
+    return a + b
 
 
-if  __name__ == "__main__":
+def div(a, b):
+    return a / b
+
+
+if __name__ == "__main__":
     print("Hello Python")
     print("hi pycharm")
     print("Good Luck")
     x = 10
     y = 5
-    res = add(x, y)
-    print("add({}, {}) result => {}" .format(x, y, res))
+    print("add({}, {}) result => {}\ndiv({}, {}) result => {}".format(x, y, add(x,y), x, y, div(x,y)))
