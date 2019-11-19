c1f6376544502c30155dd82588a0b80ae9226bd0
Builder <kgdfather@gmail.com>
2019-11-19 10:27:46 +0900

    mul(a, b)함수 추가, main print문 변경

--- chapter_git/hello_python.py
+++ chapter_git/hello_python.py
@@ -2,11 +2,17 @@ def add(a, b):
     return a+b
 
 
+def mul(a, b):
+    return a*b
+
+
 if  __name__ == "__main__":
     print("Hello Python")
     print("hi pycharm")
     print("Good Luck")
     x = 10
     y = 5
-    res = add(x, y)
-    print("add({}, {}) result => {}" .format(x, y, res))
+    print("add({}, {}) result => {}\nmul({}, {}) result => {}" .format(x, y, add(x, y), x, y, mul(x, y)))
+
+
+
