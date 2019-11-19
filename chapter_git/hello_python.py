53bca5826ce5a2ebeb9b4ebe16ab15627b713699
인공지능 <kgdfather@gmail.com>
2019-11-19 10:15:51 +0900

    add() 함수 추가 및 테스트 확

--- chapter_git/hello_python.py
+++ chapter_git/hello_python.py
@@ -1,4 +1,12 @@
+def add(a, b):
+    return a+b
+
+
 if  __name__ == "__main__":
     print("Hello Python")
     print("hi pycharm")
     print("Good Luck")
+    x = 10
+    y = 5
+    res = add(x, y)
+    print("add({}, {}) result => {}" .format(x, y, res))
