0e2790ebef512b309d2f199530e356961fd3e6a6
인공지능 <kgdfather@gmail.com>
2019-11-22 10:36:35 +0900

    create_dict() 구현 및 student_dict 구조 설계 완료

--- student_management/exam05.py
+++ student_management/exam05.py
@@ -4,13 +4,24 @@ def avg_insert(_list):
         list_list.append(avg)
 
 
+def create_dict(_dict, _list):
+    i = 1
+    for list_list in _list:
+        _dict[i] = list_list
+        i += 1
+
+
 if __name__ == "__main__":
     student_list = [["김승영", 90, 90, 90], ["정창도", 50, 60, 70], ["지재삼", 80, 80, 20]]
     print(student_list)
 
     avg_insert(student_list)
     print(student_list)
-    # student_dict = {"1":["김승영", 90, 90, 90, 270], "2":["정창도"]}
+
+    student_dict = {}
+    create_dict(student_dict, student_list)
+    print(type(student_dict))
+    print(student_dict)
 
 
     # with open("student_list", "wt") as f:
