6a29d07c7fa172dca49bc5e1fe9efc2103e4d35d
인공지능 <kgdfather@gmail.com>
2019-11-22 11:47:08 +0900

    fwrite_dict() 수정 및 fread_dict() 구현. 구현 된 모든 함수들에 대한 test code 작성 및 실행 결과 확인 완료

--- student_management/exam05.py
+++ student_management/exam05.py
@@ -2,6 +2,7 @@ def avg_insert(_list):
     for list_list in _list:
         avg = (list_list[1] + list_list[2] + list_list[3]) / 3
         list_list.append(avg)
+    print("avg_insert() done!\n")
 
 
 def create_dict(_dict, _list):
@@ -9,25 +10,42 @@ def create_dict(_dict, _list):
     for list_list in _list:
         _dict[i] = list_list
         i += 1
+    print("create_dict() done!\n")
 
 
 def fwrite_dict(filename, _dict):
     with open(filename, "wt") as f:
         for _tuple in _dict.items():
-            f.write("{} {}\n".format(_tuple[0], _tuple[1]))
+            f.write(f"{_tuple[0]} {_tuple[1][0]} {_tuple[1][1]} {_tuple[1][2]} {_tuple[1][3]} {_tuple[1][4]}\n")
+    print("fwrite_dict() done!\n")
+
+
+def fread_dict(filename, _dict):
+    with open(filename, "rt") as f:
+        temp_list = []
+        for line in f:
+            temp_list.extend(line.split())
+            _dict[int(temp_list[0])] = [temp_list[1], int(temp_list[2]), int(temp_list[3]),
+                                   int(temp_list[4]), float(temp_list[5])]
+            temp_list.clear()
+    print("fread_dict() done!\n")
 
 
 if __name__ == "__main__":
     student_list = [["김승영", 90, 90, 90], ["정창도", 50, 60, 70], ["지재삼", 80, 80, 20]]
-    print(student_list)
+    print("print(student_list)", student_list, sep='\n', end='\n\n')
 
     avg_insert(student_list)
-    print(student_list)
+    print("print(student_list)", student_list, sep='\n', end='\n\n')
 
     student_dict = {}
     create_dict(student_dict, student_list)
-    print(type(student_dict))
-    print(student_dict)
+    print("print(student_dict)", student_dict, sep='\n', end='\n\n')
 
     fwrite_dict("student_list.txt", student_dict)
 
+    empty_dict = {}
+    print("print(empty_dict)", empty_dict, sep='\n', end='\n\n')
+
+    fread_dict("student_list.txt", empty_dict)
+    print("print(empty_dict)", empty_dict, sep='\n', end='\n\n')
