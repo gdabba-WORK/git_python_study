66501d8d908db22fd6a2a3224e89e191e098fe02
인공지능 <kgdfather@gmail.com>
2019-11-22 10:48:02 +0900

    fwrite_dict() 구현 및 student_list.txt 파일 생성 가능 확인 완료

--- student_management/exam05.py
+++ student_management/exam05.py
@@ -11,6 +11,12 @@ def create_dict(_dict, _list):
         i += 1
 
 
+def fwrite_dict(filename, _dict):
+    with open(filename, "wt") as f:
+        for _tuple in _dict.items():
+            f.write("{} {}\n".format(_tuple[0], _tuple[1]))
+
+
 if __name__ == "__main__":
     student_list = [["김승영", 90, 90, 90], ["정창도", 50, 60, 70], ["지재삼", 80, 80, 20]]
     print(student_list)
@@ -23,6 +29,5 @@ if __name__ == "__main__":
     print(type(student_dict))
     print(student_dict)
 
+    fwrite_dict("student_list.txt", student_dict)
 
-    # with open("student_list", "wt") as f:
-    #     pass
